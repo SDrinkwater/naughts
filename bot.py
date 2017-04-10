9c90b9ee2c8d7f4ea8791190dc549168e089c9a1
Samuel Drinkwater <samueldrinkwater@gmail.com>
2017-04-10 20:57:24 +1200

    Add random move selection to the bot. When multiple moves are equally effective, one is chosen at random.

--- bot.py
+++ bot.py
@@ -1,68 +1,84 @@
-# Move 1: Go in a corner.
-# Move 2: If the other player did not go there then go in the opposite corner to move 1. Otherwise go in a free corner.
-# Move 3: If there are two Xs and a space in a line (in any order) then go in that space. Otherwise if there are two Os and a space in a line then go in that space. Otherwise go in a free corner.
-# Move 4: If there are two Xs and a space in a line (in any order) then go in that space. Otherwise if there are two Os and a space in a line then go in that space. Otherwise go in a free corner.
-# Move 5: Go in the free space.
+from random import randint
+
 
 class Bot:
+    """A simple naughts and crosses bot.
 
-	def __init__(self, player):
-		self.player = player
+    This bot uses the following process to find a move:
+        Move 1: Go in a corner.
+        Move 2: If the other player did not go there then go in the opposite corner to move 1.
+        Otherwise go in a free corner.
+        Move 3: If there are two Xs and a space in a line (in any order) then go in that space.
+        Otherwise if there are two Os and a space in a line then go in that space. Otherwise go in a free corner.
+        Move 4: If there are two Xs and a space in a line (in any order) then go in that space.
+        Otherwise if there are two Os and a space in a line then go in that space. Otherwise go in a free corner.
+        Move 5: Go in the free space.
 
-	def get_all_lines(self, board):
-		lines = []
-		lines.append([[board[0][0], board[0][1], board[0][2]], {0: [0,0], 1: [0,1], 2: [0,2]}])
-		lines.append([[board[1][0], board[1][1], board[1][2]], {0: [1,0], 1: [1,1], 2: [1,2]}])
-		lines.append([[board[2][0], board[2][1], board[2][2]], {0: [2,0], 1: [2,1], 2: [2,2]}])
-		lines.append([[board[0][0], board[1][0], board[2][0]], {0: [0,0], 1: [1,0], 2: [2,0]}])
-		lines.append([[board[0][1], board[1][1], board[2][1]], {0: [0,1], 1: [1,1], 2: [2,1]}])
-		lines.append([[board[0][2], board[1][2], board[2][2]], {0: [0,2], 1: [1,2], 2: [2,2]}])
-		lines.append([[board[0][0], board[1][1], board[2][2]], {0: [0,0], 1: [1,1], 2: [2,2]}])
-		lines.append([[board[0][2], board[1][1], board[2][0]], {0: [0,2], 1: [1,1], 2: [2,0]}])
-		return lines
+    Attributes:
+        player: A string indicating the player controlled by the bot.
+    """
 
-	def block_opponent(self, line):
-		free_moves = [i for i,x in enumerate(line) if x == ' ']
-		moves_taken_by_player = [j for j,x in enumerate(line) if x == self.player]
-		moves_taken_by_opponent = [j for j,x in enumerate(line) if x != self.player and x != ' ']
+    def __init__(self, player):
+        self.player = player
 
-		if len(free_moves) == 1 and len(moves_taken_by_opponent) == 0:
-			return free_moves[0], True
+    @staticmethod
+    def get_all_lines(board):
+        lines = []
+        lines.append([[board[0][0], board[0][1], board[0][2]], {0: [0, 0], 1: [0, 1], 2: [0, 2]}])
+        lines.append([[board[1][0], board[1][1], board[1][2]], {0: [1, 0], 1: [1, 1], 2: [1, 2]}])
+        lines.append([[board[2][0], board[2][1], board[2][2]], {0: [2, 0], 1: [2, 1], 2: [2, 2]}])
+        lines.append([[board[0][0], board[1][0], board[2][0]], {0: [0, 0], 1: [1, 0], 2: [2, 0]}])
+        lines.append([[board[0][1], board[1][1], board[2][1]], {0: [0, 1], 1: [1, 1], 2: [2, 1]}])
+        lines.append([[board[0][2], board[1][2], board[2][2]], {0: [0, 2], 1: [1, 2], 2: [2, 2]}])
+        lines.append([[board[0][0], board[1][1], board[2][2]], {0: [0, 0], 1: [1, 1], 2: [2, 2]}])
+        lines.append([[board[0][2], board[1][1], board[2][0]], {0: [0, 2], 1: [1, 1], 2: [2, 0]}])
+        return lines
 
-		if len(free_moves) == 1 and len(moves_taken_by_player) == 0:
-			return free_moves[0], False
+    def block_opponent(self, line):
+        free_moves = [i for i, x in enumerate(line) if x == ' ']
+        moves_taken_by_player = [j for j, x in enumerate(line) if x == self.player]
+        moves_taken_by_opponent = [j for j, x in enumerate(line) if x != self.player and x != ' ']
 
-		return -1, False
+        if len(free_moves) == 1 and len(moves_taken_by_opponent) == 0:
+            return free_moves[0], True
 
-	def find_corner(self, board):
-		corners = [[0,0], [0,2], [2,0], [2,2]]
-		for corner in corners:
-			if board[corner[0]][corner[1]] == self.player and board[(corner[0]+2)%4][(corner[1]+2)%4] == ' ':
-				return [(corner[0]+2)%4,(corner[1]+2)%4]
-			if board[corner[0]][corner[1]] == ' ':
-				return corner
+        if len(free_moves) == 1 and len(moves_taken_by_player) == 0:
+            return free_moves[0], False
 
-	def find_free_space(self, board):
-		for x, line in enumerate(board):
-			for y, move in enumerate(line):
-				if move == ' ':
-					return [x, y]
+        return -1, False
 
+    @staticmethod
+    def find_corners(board):
+        corners = [[0, 0], [2, 2], [0, 2], [2, 0]]
+        free_corners = []
+        for corner in corners:
+            if board[corner[0]][corner[1]] == ' ':
+                free_corners.append(corner)
 
-	def move(self, board):
-		move = []
-		for line in self.get_all_lines(board):
-			new_move, win = self.block_opponent(line[0])
-			if win:
-				return line[1][new_move]
+        return free_corners
 
-			if new_move >= 0:
-				move = line[1][new_move]
+    @staticmethod
+    def find_free_space(board):
+        for x, line in enumerate(board):
+            for y, move in enumerate(line):
+                if move == ' ':
+                    return [x, y]
 
-		if len(move) > 0:
-			return move
+    def move(self, board):
+        move = []
+        for line in self.get_all_lines(board):
+            new_move, win = self.block_opponent(line[0])
+            if win:
+                return line[1][new_move]
 
-		corner = self.find_corner(board)
-		if corner is not None:
-			return corner
-		return self.find_free_space(board)
+            if new_move >= 0:
+                move = line[1][new_move]
+
+        if len(move) > 0:
+            return move
+
+        corners = self.find_corners(board)
+        if len(corners) > 0:
+            idx = randint(0, len(corners) - 1)
+            return corners[idx]
+        return self.find_free_space(board)
--- naughts.py
+++ naughts.py
@@ -4,113 +4,113 @@ from bot import Bot
 
 
 def coordinate_to_board_position(coordinate):
-	x_plane = {'A': 0, 'B': 1, 'C': 2}
-	y_plane = {'1': 0, '2': 1, '3': 2}
+    x_plane = {'A': 0, 'B': 1, 'C': 2}
+    y_plane = {'1': 0, '2': 1, '3': 2}
 
-	return [y_plane[coordinate[1]], x_plane[coordinate[0]]]
+    return [y_plane[coordinate[1]], x_plane[coordinate[0]]]
 
 
 def game_over(moves):
-	if moves[0][0] != ' ' and moves[0][0] == moves[0][1] == moves[0][2]:
-		return moves[0][0]
-	if moves[0][0] != ' ' and moves[0][0] == moves[1][0] == moves[2][0]:
-		return moves[0][0]
-	if moves[0][0] != ' ' and moves[0][0] == moves[1][1] == moves[2][2]:
-		return moves[0][0]
-	if moves[0][2] != ' ' and moves[0][2] == moves[1][2] == moves[2][2]:
-		return moves[0][2]
-	if moves[0][2] != ' ' and moves[0][2] == moves[1][1] == moves[2][0]:
-		return moves[0][2]
-	if moves[2][0] != ' ' and moves[2][0] == moves[2][1] == moves[2][2]:
-		return moves[2][0]
-	if moves[1][0] != ' ' and moves[1][0] == moves[1][1] == moves[1][2]:
-		return moves[1][0]
+    if moves[0][0] != ' ' and moves[0][0] == moves[0][1] == moves[0][2]:
+        return moves[0][0]
+    if moves[0][0] != ' ' and moves[0][0] == moves[1][0] == moves[2][0]:
+        return moves[0][0]
+    if moves[0][0] != ' ' and moves[0][0] == moves[1][1] == moves[2][2]:
+        return moves[0][0]
+    if moves[0][2] != ' ' and moves[0][2] == moves[1][2] == moves[2][2]:
+        return moves[0][2]
+    if moves[0][2] != ' ' and moves[0][2] == moves[1][1] == moves[2][0]:
+        return moves[0][2]
+    if moves[2][0] != ' ' and moves[2][0] == moves[2][1] == moves[2][2]:
+        return moves[2][0]
+    if moves[1][0] != ' ' and moves[1][0] == moves[1][1] == moves[1][2]:
+        return moves[1][0]
 
 
 def print_board(moves):
-	y_coords_to_line = {1: 0, 4: 1, 7: 2}
-	line_to_x_coords = {0: 3, 1: 8, 2: 13}
-	board = [
-		'      |   |     ',
-		'1     |   |     ',
-		' _____|___|_____', 
-		'      |   |     ', 
-		'2     |   |     ', 
-		' _____|___|_____',
-		'      |   |     ', 
-		'3     |   |     ', 
-		'      |   |     ',
-		'   A    B    C  ',
-	]
+    y_coords_to_line = {1: 0, 4: 1, 7: 2}
+    line_to_x_coords = {0: 3, 1: 8, 2: 13}
+    board = [
+        '      |   |     ',
+        '1     |   |     ',
+        ' _____|___|_____',
+        '      |   |     ',
+        '2     |   |     ',
+        ' _____|___|_____',
+        '      |   |     ',
+        '3     |   |     ',
+        '      |   |     ',
+        '   A    B    C  ',
+    ]
 
-	for idx, line in enumerate(board):
-		line_as_list = list(line)
-		if idx in [1, 4, 7]:
-			for idx_moves, move in enumerate(moves[y_coords_to_line[idx]]):
-				line_as_list[line_to_x_coords[idx_moves]] = move
+    for idx, line in enumerate(board):
+        line_as_list = list(line)
+        if idx in [1, 4, 7]:
+            for idx_moves, move in enumerate(moves[y_coords_to_line[idx]]):
+                line_as_list[line_to_x_coords[idx_moves]] = move
 
-		print(''.join(line_as_list))
+        print(''.join(line_as_list))
 
 
 def get_valid_move(player, moves):
-	while True:
-		try:
-			response = input('Player {0}\'s turn: '.format(player)).upper()
-			if response == 'Q':
-				sys.exit(0)
-			print(response)
-			if len(response) == 2 and response[0] >= 'A' and response[0] <= 'C'	and int(response[1]) >= 1 and int(response[1]) <= 3:
-				move = coordinate_to_board_position(response)
-				if moves[move[0]][move[1]] != ' ':
-					raise ValueError('That move is already taken!')
-				else:
-					return move
-			raise ValueError('Please enter a valid move, i.e., "A1"\n')
-		except ValueError as error:
-			print(error)
-			continue
+    while True:
+        try:
+            response = input('Player {0}\'s turn: '.format(player)).upper()
+            if response == 'Q':
+                sys.exit(0)
+            print(response)
+            if len(response) == 2 and 'A' <= response[0] <= 'C' and 0 < int(response[1]) < 4:
+                move = coordinate_to_board_position(response)
+                if moves[move[0]][move[1]] != ' ':
+                    raise ValueError('That move is already taken!')
+                else:
+                    return move
+            raise ValueError('Please enter a valid move, i.e., "A1"\n')
+        except ValueError as error:
+            print(error)
+            continue
 
 
-def get_input(message, options = ['y', 'n']):
-	while True:
-		response = input(message)
-		if len(options) > 0:
-			if response in options:
-				return response
-		else:
-			return response
+def get_input(message, options=['y', 'n']):
+    while True:
+        response = input(message).lower()
+        if len(options) > 0:
+            if response in options:
+                return response
+        else:
+            return response
 
 
 def play_game(players):
-	moves_taken = 0
-	moves = [
-		[' ', ' ', ' '],
-		[' ', ' ', ' '],
-		[' ', ' ', ' '],
-	]
-	b = Bot('X')
-	winner = None
+    moves_taken = 0
+    moves = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+    ]
+    b = Bot('X')
+    winner = None
 
-	while moves_taken < 9:
-		player = players[moves_taken % 2]
-		print_board(moves)
-		print('Moves:', moves_taken)
+    while moves_taken < 9:
+        player = players[moves_taken % 2]
+        print_board(moves)
+        print('Moves:', moves_taken)
 
-		if player == 'X':
-			move = b.move(moves)
-		else:
-			move = get_valid_move(player, moves)
+        if player == 'X':
+            move = b.move(moves)
+        else:
+            move = get_valid_move(player, moves)
 
-		moves[int(move[0])][int(move[1])] = player
-		moves_taken += 1
-		winner = game_over(moves)
-		if winner:
-			break
-	print_board(moves)
-	if winner:
-		print('\n{0} wins the game!\n'.format(player))
-	else:
-		print('\nDraw!\n')
+        moves[int(move[0])][int(move[1])] = player
+        moves_taken += 1
+        winner = game_over(moves)
+        if winner:
+            break
+    print_board(moves)
+    if winner:
+        print('\n{0} wins the game!\n'.format(player))
+    else:
+        print('\nDraw!\n')
 
 
 def signal_handler(signal, frame):
@@ -119,12 +119,12 @@ def signal_handler(signal, frame):
 
 
 def main():
-	players = ['X', 'O']
+    players = ['X', 'O']
 
-	while True:
-		play_game(players)
-		if get_input('Play again? (y/n) ') == 'n':
-			break
+    while True:
+        play_game(players)
+        if get_input('Play again? (y/n) ') == 'n':
+            break
 
 
 signal.signal(signal.SIGINT, signal_handler)
